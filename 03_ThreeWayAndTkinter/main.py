a66a487f0247b49669f3ecbf35502953a0a55dcf
vkurilin <vkurilin@smartdec.ru>
2021-03-14 14:48:07 +0300

    dummy buttons implementation

--- 03_ThreeWayAndTkinter/main.py
+++ 03_ThreeWayAndTkinter/main.py
@@ -1,18 +1,49 @@
 import tkinter as tk
+from dataclasses import dataclass
+import random
+from sympy.combinatorics.permutations import Permutation
 
 
-class HelloWorld(tk.Frame):
-    def __init__(self, parent):
-        super(HelloWorld, self).__init__(parent)
+@dataclass
+class GameData:
+    rows: int
+    columns: int
+    permutation: list[int]
 
-        self.label = tk.Label(self, text="Hello, World!")
-        self.label.pack(padx=20, pady=20)
+    def check_parity_of_permutation(self) -> bool:
+        return True
+
+game_data = GameData(4, 4, [])
 
 
-if __name__ == "__main__":
-    root = tk.Tk()
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.create_widgets()
+        self.pack()
 
-    main = HelloWorld(root)
-    main.pack(fill="both", expand=True)
+    def create_widgets(self):
+        for i in range(game_data.rows + 1):
+            self.grid_rowconfigure(i, weight=1, uniform='row')
+        for i in range(game_data.columns):
+            self.grid_columnconfigure(i, weight=1, uniform='col')
 
-    root.mainloop()
+        for row in range(game_data.rows):
+            for column in range(game_data.columns):
+                button = tk.Button(self, text=str(row)+str(column))
+                button['command'] = lambda button=button, row=row, column=column: print(f'Hey, {row, column}')
+                button.grid(row=row, column=column, sticky=tk.N + tk.E + tk.S + tk.W)
+
+        new = tk.Button(self, text='New', command=lambda: print('New'))
+        quit = tk.Button(self, text='Exit', command=exit)
+        new.grid(row=game_data.rows, column=0)
+        quit.grid(row=game_data.rows, column=1)
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
